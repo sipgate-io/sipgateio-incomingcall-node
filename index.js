697bf0ff774761373d039c649b7ecb23be21fb9d
Daniel Knerr <knerr@sipgate.de>
2022-12-29 14:45:11 +0100

    :sparkles: use GCP App Engine port, if provided
    Co-authored-by: Harun Karacuban <karacuban@sipgate.de>

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 require("dotenv").config();
 
-const port = process.env.WEBHOOK_PORT;
+const port = process.env.PORT || process.env.WEBHOOK_PORT;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
